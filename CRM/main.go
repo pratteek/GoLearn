bb5e88b9dd28277f6b0c3261490540b90f48826b
prateekkhalkho <prateekkhalkho@gmail.com>
2022-07-22 15:16:12 +0530

    rest api with mux added

---
+++ CRM/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"encoding/json"
+)
+
+type student struct{
+	Name string `json:"name"`
+	Age uint	`json:"age"`
+	Branch string `json:"branch"`
+	GPA float64 `json:"gpa"`
+}
+var students =  []student{
+	{Name:"Prateek",Age:22,Branch:"CSE",GPA:10},
+	{Name:"Rohit",Age:22,Branch:"ECE",GPA:9},
+	{Name:"Nidhi",Age:22,Branch:"EEE",GPA:9.5},
+}
+func Home(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(students)
+	fmt.Println("Home Page")
+}
+func main(){
+	r := mux.NewRouter()
+	r.Path("/").Methods(http.MethodGet).HandlerFunc(Home)
+
+	log.Fatal(http.ListenAndServe(":9000",r))
+}
